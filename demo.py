78e9de529382f96820ab1ba7b097eca0200564cc
leblancdaniel <daniel.leblanc@augustusai.com>
2020-04-20 13:39:13 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,8 +1,8 @@
-import io
+import tracking as trk
 import time
 
-cam = io.CamReader(width=1280, height=720, mirror=True)
-window = io.WindowWriter("Test Window", fps=True)
+cam = trk.io.CamReader(width=1280, height=720, mirror=True)
+window = trk.io.WindowWriter("Test Window", fps=True)
 
 for image in cam:
     window.write(image)
