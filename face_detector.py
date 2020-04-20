16625af5ffc0ffe4dfd910d498f01228c83772a5
leblancdaniel <daniel.c.leblanc94@gmail.com>
2020-04-20 15:13:20 -0400

    Update face_detector.py

--- face_detector.py
+++ face_detector.py
@@ -1,33 +1,30 @@
 import torch
-
 import tracking as trk
 from tracking.detect import FaceMTCNN
 from tracking.annotate import load_osnet
 import time
+import os
 
 cam = trk.io.CamReader(width=1280, height=720, mirror=True)
 window = trk.io.WindowWriter("Test Window", fps=True)
 
+script_dir = os.path.dirname(__file__)
+model_path = os.path.join(script_dir, "models")
+osnet_path = os.path.join(model_path, "/osnet075.pth")
+
 class FaceDetector:
     def __init__(self):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        ###########################
-        # change filepaths for demo
-        ###########################
-        self.detection = FaceMTCNN("file/path/to/cameralytics/models", device=device)
-        self.features = load_osnet(
-            "file/path/to/cameralytics/models/osnet075.pth", arch="x0_75"
-        )
+        self.detection = FaceMTCNN(model_path, device=device)
+        self.features = load_osnet(osnet_path, arch="x0_75")
 
     def __call__(self, frame: Image):
         frame = self.detection(frame)
-        # No faces detected, return False
         if len(frame.objects) < 1:
             print("-----------------------NO FACES DETECTED--------------------")
             return frame
         frame = self.features(frame)
         return frame
-
 
 detector = FaceDetector()
 
@@ -41,4 +38,3 @@ for image in cam:
     
 window.close_all()
 cam.close()
-
