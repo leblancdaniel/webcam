e50c72fe00e8564c8152e1b4b0b46811f0b3d063
leblancdaniel <daniel.c.leblanc94@gmail.com>
2020-04-29 14:44:57 -0400

    Update visualize.py

--- FER/visualize.py
+++ FER/visualize.py
@@ -1,55 +1,60 @@
 """
-visualize results for test image
+Emotion Detector for object tracking pipeline
 """
-
+import os
 import numpy as np
 
 from PIL import Image
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import os
 from torch.autograd import Variable
+from torchvision import transforms
 
-import transforms as transforms
-from skimage import io
-from skimage.transform import resize
-from models import *
+import albumentations as albu 
+import vgg 
 
-cut_size = 44
+class EmotionDetector:
+    def __init__(self):
+        self.cut_size = 44
 
-transform_test = transforms.Compose([
-    transforms.TenCrop(cut_size),
-    transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
-])
+    def expression(self, frame, objects, index):
+        # apply TenCrop to image and convert each crop ToTensor
+        transform_test = transforms.Compose([
+            transforms.TenCrop(self.cut_size),
+            transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
+        ])
+        # albumentations helper function
+        def augment(aug, image):
+            return aug(image=image)['image']
+        # face bounding box coordinates as type: integer
+        box_ints = objects[index]['box'].astype(int)
+        # crop bounding box from frame, resize to 48x48, convert to Grayscale
+        crop_aug = albu.Crop(box_ints[0], box_ints[1], box_ints[2], box_ints[3])
+        img = augment(crop_aug, frame)
+        size_aug = albu.Resize(48, 48)
+        img = augment(size_aug, img)
+        img = transforms.ToPILImage()(img)
+        img = transforms.Grayscale()(img)
+        # apply TenCrop to Resized + Grayscaled faces and convert ToTensor inputs
+        inputs = transform_test(img)
 
-read_img = io.imread('images/1.jpg', as_gray=True)  # raw frames to read, convert to grayscale
-resized_img = resize(read_img, (48,48), mode='symmetric').astype(np.uint8)
+        class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
-img = resized_img[:, :, np.newaxis]
-img = np.concatenate((img, img, img), axis=2)
-img = Image.fromarray(img)
-inputs = transform_test(img)
+        net = VGG('VGG19')
+        checkpoint = torch.load('../FER/PrivateTest_model.t7',     # path to FER model
+            map_location=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+        net.load_state_dict(checkpoint['net'])
+        net.eval()
 
-class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+        ncrops, c, h, w = np.shape(inputs)
+        inputs = inputs.view(-1, c, h, w)
+        outputs = net(inputs)
+        outputs_avg = outputs.view(ncrops, -1).mean(0)  # avg over crops
 
-net = VGG('VGG19')
-checkpoint = torch.load('../FER_pytorch/PrivateTest_model.t7',     # path to model
-    map_location=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-net.load_state_dict(checkpoint['net'])
-net.eval()
+        score = F.softmax(outputs_avg)
+        _, predicted = torch.max(outputs_avg.data, 0)
+        print(score)
+        print(predicted)
 
-ncrops, c, h, w = np.shape(inputs)
-inputs = inputs.view(-1, c, h, w)
-outputs = net(inputs)
-outputs_avg = outputs.view(ncrops, -1).mean(0)  # avg over crops
-
-score = F.softmax(outputs_avg)
-_, predicted = torch.max(outputs_avg.data, 0)
-print(score)
-print(predicted)
-<<<<<<< HEAD
-print("The Expression is %s" %str(class_names[int(predicted.cpu().numpy())]))
-=======
-print("The Expression is %s" %str(class_names[int(predicted.cpu().numpy())]))
->>>>>>> 5ae64ce56ad091b1f6c19b7061014814caf4d644
+        print("The Expression is %s" %str(class_names[int(predicted.cpu().numpy())]))
